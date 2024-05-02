a4c7185e933f95c3f137ace4ec412ef77c17b19a
Fr3m3l1 <fr3m3l@gmail.com>
2024-05-02 11:12:49 +0200

    updated requirements

---
+++ dashboard.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from ucimlrepo import fetch_ucirepo 
+import plotly.express as px
+
+def main():
+    st.title('Cardiotocography Dashboard')
+
+
+
+
+if __name__ == '__main__':
+    main()
