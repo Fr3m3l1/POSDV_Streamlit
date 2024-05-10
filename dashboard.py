6797023171ba5510ce38f33ed55c8a8b4b9a83ef
Fr3m3l1 <fr3m3l@gmail.com>
2024-05-10 22:12:53 +0200

    wip

--- dashboard.py
+++ dashboard.py
@@ -4,9 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from ucimlrepo import fetch_ucirepo 
 import plotly.express as px
+import plotly.graph_objects as go
 
 def main(featured_df, target_df):
     st.title('Cardiotocography Dashboard')
+
+    # show intro text
+    st.markdown('This dashboard provides an overview of a Cardiotocography dataset. The dataset contains features of fetal heart rate (FHR) and uterine contractions (UC) and the target variable Normal, Suspect, Pathologic (NSP). Feel free to explore the dataset by selecting a categorical variable from the dropdown menu below.')
 
     st.markdown('### Cardiotocography dataset overview:')
     # Number of features
@@ -26,7 +30,8 @@ def main(featured_df, target_df):
     st.write('---')
 
     # Create a array for the categorical variables with description
-    categorical_variables = ['LB: FHR baseline (beats per minute)',
+    categorical_variables = ["",
+                            'LB: FHR baseline (beats per minute)',
                              'AC: # of accelerations per second',
                              'FM: # of fetal movements per second',
                              'UC: # of uterine contractions per second',
@@ -49,18 +54,28 @@ def main(featured_df, target_df):
                              'Tendency: histogram tendency']
     
     
-    # Reset button
-    if st.button('Reset selection'):
-        categorical_variable_selection = st.selectbox('Select a categorical variable:', categorical_variables)
+    # Red Reset button 
+    if st.button('Reset selection', key='reset_selection', help='Click to reset the selection'):
+        categorical_variable_selection = st.selectbox('Select a categorical variable:', categorical_variables, index=0)
         return
 
     # select a categorical variable
-    categorical_variable_selection = st.selectbox('Select a categorical variable:', categorical_variables)
+    categorical_variable_selection = st.selectbox('Select a categorical variable:', categorical_variables, placeholder='Select a categorical variable', index=0)
 
+    # Check if the user has selected a variable
+    if categorical_variable_selection == '':
+        return
+    
     categorical_variable_selection = categorical_variable_selection.split(':')[0]
 
     # Frequency distribution as bar chart grouped by NSP
-    fig = px.bar(featured_df.join(target_df), x=categorical_variable_selection, color='NSP', barmode='group', title=f'Frequency distribution of {categorical_variable_selection} grouped by NSP')
+    fig = px.histogram(featured_df.join(target_df), x=categorical_variable_selection, color='NSP_Label', title=f'Frequency distribution of {categorical_variable_selection} grouped by NSP')
+    st.plotly_chart(fig, use_container_width=True)
+
+    fig = go.Figure()
+    for nsp in target_df['NSP_Label'].unique():
+        fig.add_trace(go.Histogram(x=featured_df[target_df['NSP_Label'] == nsp][categorical_variable_selection], name=nsp))
+    fig.update_layout(title=f'Frequency distribution of {categorical_variable_selection} grouped by NSP', barmode='overlay')
     st.plotly_chart(fig, use_container_width=True)
 
 
@@ -75,6 +90,9 @@ def loaddata():
     # data (as pandas dataframes) 
     featured_df = cardiotocography.data.features 
     target_df = cardiotocography.data.targets
+
+    # convert NSP to categorical
+    target_df['NSP_Label'] = target_df['NSP'].map({1: 'Normal', 2: 'Suspect', 3: 'Pathologic'})
     
     return featured_df, target_df
 
